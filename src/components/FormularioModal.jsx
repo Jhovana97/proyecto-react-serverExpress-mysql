6fcfff782b8ed910e46eca0155f8b4e65c154d1a
Jhovana <jhovanapatty95@gmail.com>
2025-08-13 18:47:04 -0400

    actualizacion del boton

--- src/components/FormularioModal.jsx
+++ src/components/FormularioModal.jsx
@@ -51,7 +51,7 @@ export default function FormularioModal({ onClose, curso }) {
                         type="submit"
                         className="bg-indigo-600 hover:bg-indigo-700 text-white px-4 py-2 rounded"
                     >
-                        consulta ahora
+                        Consulta ahora mismo
                     </button>
                 </form>
             </div>
